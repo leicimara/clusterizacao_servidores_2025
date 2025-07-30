d3e798a059a1231ac156d18645d9e828badb2545
leicimara <atenas@DESKTOP-K01A99A.localdomain>
2025-07-29 21:37:22 -0300

    meu primeiro commit explicando o que fiz na primeira aula de clusterizacao de servidores, comentarios no codigo ola_mundo.c

--- ola_mundo.c
+++ ola_mundo.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <mpi.h>
+/*
+Relatorio
+
+Foi feita a instalacao atraves do terminal utilizando o sudo apt install wsl, depois intalado as bibliotecas. Apos foi feito alguns
+testes.
+O codigo utilizado na pratica tem por função rodar o comando para ranquear o numero de processos contando com o tamanho de
+ nucleos possuinte, o codigo mostra diferentes resultados, mas todos gerados ao mesmos momento. Mostrando como funciona a
+clusterizacao.
+*/
 
 int main(int argc, char *argv[]) {
     int rank, size;
